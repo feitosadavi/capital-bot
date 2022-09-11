034149ecac1930d2cf227e63554390167d70a4fe
feitosadavi <davifeitosa.dev@protonmail.com>
2022-09-11 05:14:42 -0300

    remove sudo from sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 if ! [ -x "$(command -v google-chrome)" ]; then
-  command wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && npm run start
+  command wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && dpkg -i google-chrome-stable_current_amd64.deb && npm run start
   exit
 else
   command npm run prod
